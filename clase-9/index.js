f44b1a8ab72a26cd23c6cfded063ea14da286e98
Sadax25 <gdelucchi@me.com>
2022-09-18 20:02:04 -0300

    Adding clase-9

---
+++ clase-9/index.js
@@ -0,0 +1,18 @@
+// // ================ EVENTO CLICK  ================
+// let botonUno = document.getElementById("btnEventoUno")
+// let botonDos = document.getElementById("btnEventoDos")
+
+// botonUno.addEventListener("click", () => {
+//     alert("Se oprimió el boton uno")
+// })
+
+// botonDos.onclick = () => {
+//     alert("Se oprimió el botón dos")
+// }
+
+// function mostrarAlert() {
+//     alert("Se oprimió el botón tres")
+// }
+
+
+
